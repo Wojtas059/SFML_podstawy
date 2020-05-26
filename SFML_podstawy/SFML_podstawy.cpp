fe8a6104ef349562323b57baa9546d9f8a518acc
Wojtas059 <wojtas.maj34@gmail.com>
2020-05-26 13:35:41 +0200

    pierwsza aktualizacja

---
+++ SFML_podstawy/SFML_podstawy.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+using namespace sf;
+using namespace std;
+
+int main()
+{
+    RenderWindow window{ VideoMode{800,800}, "SFML_podstawy" };
+
+    window.setFramerateLimit(60);
+    Event event;
+
+    while (true)
+    {
+        window.clear(Color::Black);
+        window.pollEvent(event);
+
+        if (event.type == Event::Closed)
+        {
+            window.close();
+            break;
+        }
+        window.display();
+    }
+
+
+    return 0;
+}
+
